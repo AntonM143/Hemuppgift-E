189e0f7d281e017a111c122da5a16ee4de11999e
Anton Månsson <anton.mansson@mediainstitutet.se>
2021-04-20 10:57:56 +0200

    5 små övningsuppfigter

--- index.php
+++ index.php
@@ -1,4 +1,63 @@
-<?php
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hemuppgift-E</title>
+</head>
+<body>
+<h2>hello World!</h2>
+<?php
+/* 1 */
+/* function sigill () {
+    echo "Utskrift";
+}
+sigill(); */
+/* 2 */
+/* function skrivare ($arg) {
+    print $arg;
+}
+skrivare(" wiho"); */
+/* 3 */
+/* function interaktiv_saga ($hero, $villain){
+    echo "I den farliga staden vaknar $hero och äter gröt. Sedan fångar den $villain och räddar dagen.";
+}
+interaktiv_saga("Pelle", "Olle"); */
+
+/* 4 */
+/* $sagodetaljer = array ('stadsbeskrivning'=>'Glada', 'hjältenamn'=>'Olle', 'skurknamn'=>'Andors');
+function superinteraktiv_saga($sagodetaljer){
+    echo 'I den' . $sagodetaljer['stadsbeskrivning'] . ' staden vaknar' . $sagodetaljer['hjältenamn'] . ' och äter gröt. Sedan fångar den ' . $sagodetaljer['skurknamn'] . ' och räddar dagen.';
+
+}
+superinteraktiv_saga($sagodetaljer) */
+
+
+/* 5 */
+/* function valfritt_superinteraktiv_saga ($sagodetaljer) {
+   
+
+    if(empty($sagodetaljer)){
+        $sagodetaljer = array('stadsbeskrivning'=>' farliga',
+        'hjältenamn'=>' Superkatten',
+        'skurknamn'=> ' Gurkmonstret' );
+    }
+
+        echo 'I den ' . $sagodetaljer['stadsbeskrivning'] . ' staden vaknar ' . $sagodetaljer['hjältenamn'] . ' och äter gröt. Sedan fångar den ' . $sagodetaljer['skurknamn'] . ' och räddar dagen.';
+
+    
+
+}; */
+/* kallar på tom variabel/array */
+/* valfritt_superinteraktiv_saga($hej); */
+/* ändrar värdena */
+/* valfritt_superinteraktiv_saga($sagodetaljer = array('stadsbeskrivning'=>' SOLIGA',
+ 'hjältenamn'=>' HONDEN',
+'skurknamn'=>' ANDORS'  ));
+ */
 
 ?>
+</body>
+</html>
